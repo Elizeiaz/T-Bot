d321cd86dc112b9189d82002c303f813a8c6e10b
Vladislav <homevoika@mail.ru>
2020-09-22 01:56:36 +0500

    core

---
+++ src/BotCore.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BotCore {
+    public static String input() {
+        Scanner scan = new Scanner(System.in);
+        return scan.nextLine();
+    }
+
+    public static void output(String message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        while (true) {
+
+        }
+    }
+}
