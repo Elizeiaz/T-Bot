d1bdcad5fa11315bbefb74c10d739557ad97bb2d
Vladislav <homevoika@mail.ru>
2020-10-05 21:37:47 +0500

    read

--- src/сore/FileHandler.java
+++ src/сore/FileHandler.java
@@ -1,12 +1,27 @@
 package сore;
 
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class FileHandler {
     public void writeFile(String path, String text) throws IOException {
         FileOutputStream out = new FileOutputStream(path);
         out.write(text.getBytes());
         out.close();
+    }
+
+    public String readFile(String path) throws IOException {
+        FileReader read = new FileReader(path);
+        Scanner scan = new Scanner(read);
+        String result = "";
+
+        while(scan.hasNextLine()){
+            result += scan.nextLine();
+        }
+
+        read.close();
+        return result;
     }
 }
